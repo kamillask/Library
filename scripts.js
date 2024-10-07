5194bbdfb1b81be488344772a548f607a63e6078
kamillask <kamil.laskowski86@qmail.cuny.edu>
2024-10-07 15:32:27 -0400

    Added starting HTML, CSS, and JS.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,46 @@
 const myLibrary = [];
+const bookContainer = document.querySelector(".bookContainer");
+const displayBooksButton = document.querySelector("#display");
 
-function Book(){
 
+
+function Book(name, author, pages){
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
 }
 
-function addBookToLibrary(){
-    
+const harryPotter = new Book("Azkaban", "JK Rowling", 347);
+const help = new Book("Atomic Habits", "Some Guy", 677);
+const whateverBook = new Book("48 Laws of Power", "Another Guy", 1010);
+
+function addBookToLibrary(book){
+    myLibrary.push(book);
 }
+
+addBookToLibrary(harryPotter);
+addBookToLibrary(help);
+addBookToLibrary(whateverBook);
+
+console.log(myLibrary);
+
+function displayBooks(){
+    for(let book in myLibrary){
+        alert(book.name);
+        const bookCard = document.createElement("div");
+        const bookName = document.createElement("div");
+        bookName.textContent = book.name;
+        const bookAuthor = document.createElement("p");
+        bookAuthor.textContent = book.author;
+        const bookPages = document.createElement("p");
+        bookPages.textContent = book.pages;
+        bookCard.appendChild(bookName);
+        bookCard.appendChild(bookAuthor);
+        bookCard.appendChild(bookPages);
+        bookContainer.appendChild(bookCard);
+    }
+}
+
+displayBooksButton.addEventListener("click", () => {
+    displayBooks();
+});
